07745dfccb7cd94f925da3cda39eb54cce552b0b
baekcCode <baekcode@gmail.com>
2021-11-20 00:42:39 +0900

    feat : 버튼 컴포넌트 적용

--- client/src/component/intro/index.tsx
+++ client/src/component/intro/index.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState } from 'react';
 
 import { difficulty } from '../../constants';
 import { TValue } from '../../constants/types';
+import Button from '../button';
 import Difficulty from '../difficulty';
 import { Container, Title } from './style';
 
@@ -17,7 +18,7 @@ export default function Intro(): ReactElement {
         <span>퀴즈를 풀어보세요!</span>
       </Title>
       <Difficulty selectedDifficulty={selectedDifficulty} difficulty={difficulty} handleChange={handleDifficulty} />
-      <button>퀴즈 풀기</button>
+      <Button buttonType={'primary'} children={'퀴즈 풀기'} />
     </Container>
   );
 }
