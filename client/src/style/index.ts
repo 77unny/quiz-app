1f933bde3550913127386419ddad81a786dd007b
baekcCode <baekcode@gmail.com>
2021-11-19 20:43:52 +0900

    feat : reset css 추가

--- client/src/style/index.ts
+++ client/src/style/index.ts
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import tw from 'twin.macro';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@200;300;400;500;600;700&display=swap');
+  :lang(ko){
+    font-family: 'IBM Plex Sans KR', sans-serif;
+  }
   ${reset}
+
+  * {
+    ${tw`break-words`}
+  }
+
+  html, body {
+    ${tw`bg-backgroundColor`}
+  }
+
+
 `;
