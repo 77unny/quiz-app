48f8d0df76efbcf665df09ad8bcfdddfa529e847
baekcCode <baekcode@gmail.com>
2021-11-20 00:43:14 +0900

    modify : component 코딩 스타일 수정

--- client/src/component/difficulty/index.tsx
+++ client/src/component/difficulty/index.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react';
 import { Container, RadioButtonLabel } from './style';
 import { IDifficultyProps, IRadioButtonProps } from './types';
 
-const RadioButton = ({ label, value, isChecked, handleChange }: IRadioButtonProps) => {
+function RadioButton({ label, value, isChecked, handleChange }: IRadioButtonProps) {
   const onChange = () => handleChange(value);
   return (
     <RadioButtonLabel>
@@ -11,7 +11,7 @@ const RadioButton = ({ label, value, isChecked, handleChange }: IRadioButtonProp
       <span>{label}</span>
     </RadioButtonLabel>
   );
-};
+}
 
 export default function Difficulty({ selectedDifficulty, difficulty, handleChange }: IDifficultyProps): ReactElement {
   return (
