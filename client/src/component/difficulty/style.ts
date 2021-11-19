d1f443debaa7a646ef82abfef74b0b1a47a4b89e
baekcCode <baekcode@gmail.com>
2021-11-19 22:10:28 +0900

    feat : 난이도 컴포넌트 UI 스타일링

---
+++ client/src/component/difficulty/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+export const Container = styled.div`
+  ${tw`flex items-center justify-center w-full my-4 space-x-2`}
+`;
+
+export const RadioButtonLabel = styled.label`
+  ${tw`text-center`}
+  font-size:0;
+  line-height: 0;
+
+  span {
+    ${tw`block w-16 px-2 py-2 text-gray-400 text-sm rounded-lg border border-solid border-gray-200 bg-white cursor-pointer`}
+  }
+
+  input[type='radio'] {
+    ${tw`absolute w-0 h-0 opacity-0`}
+  }
+
+  input[type='radio']:checked ~ span {
+    ${tw`font-semibold text-white border border-solid  border-subColor bg-subColor`}
+  }
+`;
