d23a01365932c61c3667dbc9f550e2aa3cb1799b
baekcCode <baekcode@gmail.com>
2021-11-20 01:32:14 +0900

    feat : Button 컴포넌트 primary 타입일때 아이콘 추가

--- client/src/component/button/index.tsx
+++ client/src/component/button/index.tsx
@@ -1,6 +1,7 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import React, { ReactElement, ReactNode } from 'react';
 
-import { Button as StyledButton } from './style';
+import { Button as StyledButton, PrimaryIcon } from './style';
 
 export interface IButtonProps {
   buttonType?: 'default' | 'primary';
@@ -9,5 +10,10 @@ export interface IButtonProps {
 }
 
 export default function Button({ buttonType = 'default', disabled = false, children }: IButtonProps): ReactElement {
-  return <StyledButton buttonType={buttonType} disabled={disabled} children={children} />;
+  return (
+    <StyledButton buttonType={buttonType} disabled={disabled}>
+      {children}
+      {buttonType === 'primary' && <PrimaryIcon icon={faChevronRight} />}
+    </StyledButton>
+  );
 }
--- client/src/component/button/style.ts
+++ client/src/component/button/style.ts
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
@@ -12,5 +13,10 @@ export const Button = styled.button<{ buttonType: 'default' | 'primary'; disable
       ? tw`text-gray-300 bg-gray-100 border border-gray-200 hover:bg-gray-100 cursor-default`
       : tw` cursor-pointer`}
   
-  ${tw`h-12 text-lg rounded-lg border-solid transition ease-in`}
+  ${tw`relative h-12 text-lg rounded-lg border-solid transition ease-in`}
+`;
+
+export const PrimaryIcon = styled(FontAwesomeIcon)`
+  ${tw`absolute top-1/2 right-4`}
+  transform: translateY(-50%);
 `;
