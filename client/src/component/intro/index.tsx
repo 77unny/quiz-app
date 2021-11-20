c7a32cbc773e6db256942b09c82810481304d029
baekcCode <baekcode@gmail.com>
2021-11-20 21:51:15 +0900

    modify : 임시 데이터 처리 (todo..)
    Todo : onClick 시에 데이터 패치를해서 화면 전환 시키자.
    - 현재는 로딩시 불러오는데 화면이 보이자마자 버튼을 누르면 없는데이터라 에러뜸
    - 그래서 일단 임시로 disabled={!data} 와 같이 해둠..
    - 이렇게 해두니까 일단은 괜찮은데 난이도 선택시 UI 스타일이 변하는게 조금 ... 불편..
    - 그래서 핸들러에 데이터 패치를 해두자!

--- client/src/component/intro/index.tsx
+++ client/src/component/intro/index.tsx
@@ -48,7 +48,14 @@ export default function Intro(): ReactElement {
       <InfoMessage>
         <span>{DIFFICULTY[selectedDifficulty].label}</span> 난이도를 선택하셨습니다.
       </InfoMessage>
-      <Button onClick={handlePlayQuiz} buttonType={'primary'} children={'퀴즈 풀기'} />
+      {/*
+        Todo : onClick 시에 데이터 패치를해서 화면 전환 시키자.
+        - 현재는 로딩시 불러오는데 화면이 보이자마자 버튼을 누르면 없는데이터라 에러뜸
+        - 그래서 일단 임시로 disabled={!data} 와 같이 해둠..
+        - 이렇게 해두니까 일단은 괜찮은데 난이도 선택시 UI 스타일이 변하는게 조금 ... 불편..
+        - 그래서 핸들러에 데이터 패치를 해두자!
+      */}
+      <Button onClick={handlePlayQuiz} buttonType={'primary'} children={'퀴즈 풀기'} disabled={!data} />
     </Container>
   );
 }
