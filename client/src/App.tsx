baeaac3baffa269a324cd5a1604ed6f791e56e90
baekcCode <baekcode@gmail.com>
2021-11-20 19:13:02 +0900

    feat : quiz 진행상태에 따른 분기처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 
 import Intro from './component/intro';
+import { getQuizState } from './store/quiz/selectors';
 import { AppContainer } from './style';
 
 export default function App(): ReactElement {
-  return (
-    <AppContainer>
-      <Intro />
-    </AppContainer>
-  );
+  const { progress } = useSelector(getQuizState);
+  return <AppContainer>{progress ? <div>게임판</div> : <Intro />}</AppContainer>;
 }
