5c77eff07915f54aa164f06f84d439dbdf9fa7d3
baekcCode <baekcode@gmail.com>
2021-11-22 16:25:48 +0900

    feat : tailwind.config.js min/max height 추가

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -67,6 +67,20 @@ module.exports = {
       inset: {
         ...DESIGN_SYSTEM.SIZE,
       },
+      minHeight: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+      },
+      maxHeight: {
+        0: '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+      },
     },
   },
   variants: {
