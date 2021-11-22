f01c8fe1a6cb217a11f66edfb5a15e69d7b1d45f
baekcCode <baekcode@gmail.com>
2021-11-22 14:43:17 +0900

    feat : modal 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 import React, { ReactElement } from 'react';
-import { useSelector } from 'react-redux';
+import Modal from 'react-modal';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Intro from './component/organisms/intro';
 import Quiz from './component/organisms/quiz';
+import { setIsOpen } from './store/modal';
+import { getModalState } from './store/modal/selector';
 import { getQuizState } from './store/quiz/selectors';
 import { AppContainer } from './style';
 
 export default function App(): ReactElement {
+  const dispatch = useDispatch();
   const { progress } = useSelector(getQuizState);
-  return <AppContainer>{progress ? <Quiz /> : <Intro />}</AppContainer>;
+  const { isOpen } = useSelector(getModalState);
+
+  const onRequestClose = () => dispatch(setIsOpen(false));
+
+  return (
+    <AppContainer>
+      {progress ? <Quiz /> : <Intro />}
+      <Modal isOpen={isOpen} onRequestClose={onRequestClose} children={'This is Modal content'} />
+    </AppContainer>
+  );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -15,3 +16,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+Modal.setAppElement('#root');
