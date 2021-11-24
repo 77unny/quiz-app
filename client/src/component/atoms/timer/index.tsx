7981e7340b34113c301bed3311a2b72201125460
baekcCode <baekcode@gmail.com>
2021-11-24 12:43:09 +0900

    refactor : 재렌더링 방지를 위해 시간 store 구현으로 인한 타이머 리팩토링

--- client/src/component/atoms/timer/index.tsx
+++ client/src/component/atoms/timer/index.tsx
@@ -1,20 +1,41 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { setElapsedTime } from '../../../store/quiz';
+import { getQuizState } from '../../../store/quiz/selectors';
+import { setTime } from '../../../store/timer';
+import { getTimeState } from '../../../store/timer/selector';
+import { timeCalculator } from '../../../utils';
 import { Container } from './style';
 
-export default function Timer({ prefix, time }: { prefix?: string; time: number }): ReactElement {
-  const min =
-    Math.floor(time / 60) < 10
-      ? '0' +
-        Math.floor(time / 60)
-          .toString()
-          .slice(-2)
-      : Math.floor(time / 60);
-  const sec = time % 60 < 10 ? '0' + (time % 60).toString().slice(-2) : time % 60;
+export default React.memo(function Timer({ prefix }: { prefix?: string }): ReactElement {
+  const dispatch = useDispatch();
+  const { time } = useSelector(getTimeState);
+  const { finished } = useSelector(getQuizState);
+  const [timer, setTimer] = useState(0);
+  const timeRef = useRef(timer);
+  const { min, sec } = timeCalculator(timer);
+
+  useEffect(() => {
+    timeRef.current = setTimeout(() => setTimer(timer + 1), 1000) as unknown as number;
+
+    return () => {
+      dispatch(setTime(timer));
+      clearTimeout(timeRef.current);
+    };
+  }, [timer]);
+
+  useEffect(() => {
+    if (finished) {
+      dispatch(setElapsedTime(time));
+      clearTimeout(timeRef.current);
+    }
+  }, [timer, time, finished]);
+
   return (
     <Container>
       {prefix && <span>{prefix} </span>}
       <span children={`${min}:${sec}`} />
     </Container>
   );
-}
+});
