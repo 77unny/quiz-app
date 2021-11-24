dcd544971a02061b1d13c9507e817c1157f8f2c8
baekcCode <baekcode@gmail.com>
2021-11-24 14:51:22 +0900

    feat : 소요 시간 수정 (유틸함수 사용)

--- client/src/component/molecules/history/chart/item/index.tsx
+++ client/src/component/molecules/history/chart/item/index.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react';
 
-import Timer from '../../../../atoms/timer';
+import { timeCalculator } from '../../../../../utils';
 import { TTable } from '../types';
 import { Container, Row } from './style';
 
 export default function Item({ item }: { item: TTable }): ReactElement {
+  const { min, sec } = timeCalculator(item.time);
   return (
     <Container>
       <Row>
@@ -13,9 +14,7 @@ export default function Item({ item }: { item: TTable }): ReactElement {
       </Row>
       <Row>
         <span>소요 시간</span>
-        <span>
-          <Timer time={item.time} />
-        </span>
+        <span>{`${min}:${sec}`}</span>
       </Row>
       <Row>
         <span>정답 비율</span>
