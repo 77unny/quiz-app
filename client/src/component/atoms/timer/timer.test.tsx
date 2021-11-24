f7b11b6c2b8420dec750f4c70c6d1d4693737db4
baekcCode <baekcode@gmail.com>
2021-11-24 21:31:51 +0900

    feat : Timer 테스트 케이스 추가 - 1초, 61초 케이스 추가

--- client/src/component/atoms/timer/timer.test.tsx
+++ client/src/component/atoms/timer/timer.test.tsx
@@ -2,8 +2,19 @@ import 'jest-styled-components';
 
 import { render } from '@testing-library/react';
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 
 import Timer from './index';
+
+beforeEach(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.useRealTimers();
+  jest.clearAllMocks();
+  jest.useRealTimers();
+});
 
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
@@ -12,9 +23,27 @@ jest.mock('react-redux', () => ({
 }));
 
 describe('<Timer />', () => {
-  it('prefix - 지속시간', () => {
+  it('[Timer] prefix - 지속시간', () => {
     const { container } = render(<Timer prefix={'지속시간'} />);
 
     expect(container.firstChild).toMatchSnapshot(`지속시간`);
+  });
+
+  it('[Timer] 1초 경과  - 00:01', () => {
+    const { getByText } = render(<Timer />);
+    act(() => {
+      jest.advanceTimersByTime(1000);
+    });
+
+    expect(getByText('00:01'));
+  });
+
+  it('[Timer] 61초 경과  - 01:01', () => {
+    const { getByText } = render(<Timer />);
+    act(() => {
+      jest.advanceTimersByTime(61000);
+    });
+
+    expect(getByText('01:01'));
   });
 });
